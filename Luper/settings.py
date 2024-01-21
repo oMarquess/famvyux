f9d2ddd8d8cccba5cb4d644d3f430642f7237494
oMarquess <pymasterclass@gmail.com>
2024-01-21 22:51:45 +0000

    celery-out

--- Luper/settings.py
+++ Luper/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'allauth_ui',
     'allauth',
     'allauth.account',
-    'django_celery_beat',
+    #'django_celery_beat',
     'allauth.socialaccount',
     'widget_tweaks'
 ]
@@ -106,11 +106,11 @@ DATABASES = {
 
 
 # Celery settings
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_ACCEPT_CONTENT = ['json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+# CELERY_BROKER_URL = 'redis://localhost:6379/0'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+# CELERY_ACCEPT_CONTENT = ['json']
+# CELERY_TASK_SERIALIZER = 'json'
+# CELERY_RESULT_SERIALIZER = 'json'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- ratings/tasks.py
+++ ratings/tasks.py
@@ -1,6 +1,6 @@
-from celery import shared_task
-from django.core.management import call_command
+# from celery import shared_task
+# from django.core.management import call_command
 
-@shared_task
-def run_calculate_average_rating():
-    call_command('calculateaveragerating')
+# @shared_task
+# def run_calculate_average_rating():
+#     call_command('calculateaveragerating')
